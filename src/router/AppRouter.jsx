1fe825a52215bf596b44e9e4144e0223c6716d17
Omar <omared250@gmail.com>
2022-09-18 16:32:45 +0200

    adding routes

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="about" element={ <About /> } />
+            </Routes>
+        </>
+    )
+};
