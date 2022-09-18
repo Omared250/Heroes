51b824618eb956a6f87c7052ad9371c0a61607b8
Omar <omared250@gmail.com>
2022-09-18 18:11:36 +0200

    useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true,
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             <Link 
@@ -26,13 +35,6 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
-
-                    <NavLink 
-                        className={ ({ isActive }) => `nav-item nav-link ${ isActive ? 'active' : ''}` } 
-                        to="/search"
-                    >
-                        Search
-                    </NavLink>
                 </div>
             </div>
 
@@ -44,6 +46,7 @@ export const Navbar = () => {
 
                     <button
                         className="nav-item nav-link btn"
+                        onClick={ onLogout }
                     >
                         Logout
                     </button>
