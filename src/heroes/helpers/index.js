36ae856d5b3002744df8ba4105f6489396af9770
Omar <omared250@gmail.com>
2022-09-19 12:00:43 +0200

    showing heroes list

--- src/heroes/helpers/index.js
+++ src/heroes/helpers/index.js
@@ -1,3 +1,4 @@
 
 export * from './getHeroesByPublisger';
 export * from './getHeroById';
+export * from './getHeroesByName';
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from 'query-string';
 import { useForm } from "../../hooks/useForm"
 import { HeroCard } from "../components"
+import { getHeroesByName } from "../helpers";
 
 export const SearchPage = () => {
 
@@ -9,8 +10,10 @@ export const SearchPage = () => {
     const location = useLocation();
     const { q = '' }= queryString.parse( location.search );
 
+    const heroes = getHeroesByName(q);
+
     const { searchText, onInputChange } = useForm({
-        searchText: '',
+        searchText: q,
     });
 
     const onSearchSubmit = (e) => {
@@ -59,7 +62,11 @@ export const SearchPage = () => {
                         No hero with <b>{ q }</b>
                     </div>
 
-                    {/* <HeroCard /> */}
+                    {
+                        heroes.map(hero => (
+                            <HeroCard key={ hero.id } {...hero} />
+                        ))
+                    }
                 </div>
             </div>
         </>
